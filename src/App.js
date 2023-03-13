d82cc83df08df9cf1ab475937cb619d17d103226
CASTICRI003 <ccastillo0924@gmail.com>
2023-03-13 18:12:58 -0500

    set up App to render page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PageContainer from "./components/PageContainer"
+import "./styles/App.css"
+
+
+function App() {
+  return (
+    <div id="Appcontain" className="d-flex row vh-100">
+      <PageContainer />
+    </div>
+  );
+}
+
+export default App;
