256238db6b463b0ca66469bcafbcdfd7b793519d
CASTICRI003 <ccastillo0924@gmail.com>
2023-03-13 17:54:02 -0500

    set up Contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers';
+
+function Form() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'name') {
+            setName(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!validateEmail(email) || !name || !message) {
+            setErrorMessage('Email is invalid. Or name and message field must not be empty.');
+            return;
+        }
+
+        setName('');
+        setMessage('');
+        setEmail('');
+        setErrorMessage('');
+    };
+
+    return (
+        <div id="contact" className='col'>
+        <h1>Contact Me</h1>
+        <form className="form">
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+          />
+          <input
+            value={name}
+            name="name"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="name"
+          />
+          <input
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="message"
+          />
+          <button type="button" onClick={handleFormSubmit}>
+            Submit
+          </button>
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+      </div>
+    )
+}
