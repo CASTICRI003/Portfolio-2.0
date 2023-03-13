95cb828baa281d2345ea50d45ceddb02d6af4973
CASTICRI003 <ccastillo0924@gmail.com>
2023-03-13 18:06:04 -0500

    set up Nav file and exported Contact form

---
+++ src/components/Navigation.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Header from "./elements/Header";
+
+function Navigation({ currentPage, handlePageChange }) {
+    return (
+        <header id='header-nav' className='col'>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className='container-fluid position-relative flex-lg-column vh-100'>
+        <Header />
+        <div className='collapse navbar-collapse'>
+          <ul className='navbar-nav flex-column'>
+            <li className="nav-item">
+              <a
+                href="#about"
+                onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+              >
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#portfolio"
+                onClick={() => handlePageChange('Portfolio')}
+                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+              >
+                Contact
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#resume"
+                onClick={() => handlePageChange('Resume')}
+                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+        </div>
+      </nav>
+    </header>
+    );
+}
+
+export default Navigation;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -74,5 +74,7 @@ function Form() {
           </div>
         )}
       </div>
-    )
+    );
 }
+
+export default Form;
